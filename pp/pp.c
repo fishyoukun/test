e387eea05746a12ba11b624dcff6954624be6829
fish <fishyoukun@gmail.com>
2017-02-22 18:34:20 +0800

    pointor

---
+++ pp/pp.c
@@ -0,0 +1,11 @@
+//
+// Created by fish on 2/22/17.
+//
+
+#include "pp.h"
+int main()
+{
+
+
+    int * (*p)[2](int *a);
+}
---
+++ pp/pp.h
@@ -0,0 +1,11 @@
+//
+// Created by fish on 2/22/17.
+//
+
+#ifndef TEST_PP_H
+#define TEST_PP_H
+
+#include <stdio.h>
+
+
+#endif //TEST_PP_H
